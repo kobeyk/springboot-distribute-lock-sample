e05394b4aa57c952d68fafe09ba1b979f80f28c5
yukun <437704792@qq.com>
2021-02-21 14:57:32 +0800

    消除多余引用

--- zookeeper-lock/src/main/java/com/appleyk/zk/service/impl/CommodityServiceImpl.java
+++ zookeeper-lock/src/main/java/com/appleyk/zk/service/impl/CommodityServiceImpl.java
@@ -13,8 +13,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionSynchronization;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
-
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
